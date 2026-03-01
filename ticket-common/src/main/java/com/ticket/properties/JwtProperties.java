a24a01e04fe85c160b7b9fb640ce6dd4fafaa6dc
danielchen <zhhchendaniel@gmail.com>
2026-03-02 00:17:27 +0800

    chore: initialize ticket system project baseline

---
+++ ticket-common/src/main/java/com/ticket/properties/JwtProperties.java
@@ -0,0 +1,23 @@
+package com.ticket.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "ticket.jwt")
+public class JwtProperties {
+
+    private String userSecretKey;
+
+    private long userTtl;
+
+    private String userTokenName;
+
+    private String adminSecretKey;
+
+    private long adminTtl;
+
+    private String adminTokenName;
+}
